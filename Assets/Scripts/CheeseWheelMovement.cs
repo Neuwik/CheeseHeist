5f3c3a980f1bab27a118ead5ab2c841bc4c20fe6
Dominik Neuwirth <domineu04@gmail.com>
2024-05-01 13:03:00 +0200

    Automatic Resest

--- Assets/Scripts/CheeseWheelMovement.cs
+++ Assets/Scripts/CheeseWheelMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class CheeseWheelMovement : MonoBehaviour
 {
@@ -13,10 +14,13 @@ public class CheeseWheelMovement : MonoBehaviour
     public float ForwardSpeed = 1;
     public float TurnSpeed = 1;
 
-    public Vector3 Forward;
+    public LayerMask GroundLayer;
+    public Vector3 Down = Vector3.down;
+    public Vector3 Forward = Vector3.forward;
     public GameObject WheelCenter;
     public GameObject ResetPoint;
     public Vector3 ResetPositionOffset = new Vector3(0,2,0);
+    public float AutoResetAngle = 45;
 
     void Start()
     {
@@ -29,14 +33,28 @@ public class CheeseWheelMovement : MonoBehaviour
         Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, movementTurn * TurnSpeed * Time.fixedDeltaTime, 0));
         rb.MoveRotation(deltaRotation * transform.rotation);
 
-        Forward = Vector3.Cross(transform.up, Vector3.down).normalized;
-        //Debug.DrawRay(transform.position, forward, Color.yellow, Time.fixedDeltaTime);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, GroundLayer.value))
+        {
+            Down = hit.transform.up * -1;
+        }
+        Debug.DrawRay(transform.position, Down, Color.yellow, Time.fixedDeltaTime);
+
+        Forward = Vector3.Cross(transform.up, Down).normalized;
+        Debug.DrawRay(transform.position, Forward, Color.blue, Time.fixedDeltaTime);
 
         rb.AddForce(Forward * movementForward * ForwardSpeed * Time.fixedDeltaTime * rb.mass);
 
         //Camera
         WheelCenter.transform.position = transform.position;
         WheelCenter.transform.LookAt(transform.position + Forward);
+
+        float angle = Vector3.Angle(Down, transform.up);
+        Debug.Log(angle);
+        if (angle + AutoResetAngle >= 180 || angle - AutoResetAngle <= 0)
+        {
+            ResetPosition();
+        }
     }
 
     private void OnMove(InputValue movementValue)
